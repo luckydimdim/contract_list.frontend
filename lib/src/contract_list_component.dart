0d308257cecdcbe1893a1c35417039ab85c842f2
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-01-31 16:09:37 +0300

    table row onclick

--- lib/src/contract_list_component.dart
+++ lib/src/contract_list_component.dart
@@ -1,15 +1,15 @@
+import 'dart:html';
+
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 
 import 'package:alert/alert_service.dart';
 import 'package:resources_loader/resources_loader.dart';
-import 'package:grid/grid.dart';
-import 'package:js/js.dart';
 
 @Component(selector: 'contract-list')
 @View(
     templateUrl: 'contract_list_component.html', directives: const [RouterLink])
-class ContractListComponent implements OnInit, OnDestroy {
+class ContractListComponent implements OnInit {
   static const String route_name = "ContractList";
   static const String route_path = "contractList";
   static const Route route = const Route(
@@ -21,46 +21,17 @@ class ContractListComponent implements OnInit, OnDestroy {
   final AlertService _alertService;
   final ResourcesLoaderService _resourcesLoaderService;
 
-  Grid _grid;
 
   ContractListComponent(
       this._router, this._alertService, this._resourcesLoaderService) {}
 
-  String _render(dynamic record, dynamic ind, dynamic col_ind, dynamic data) {
-    /* непонятно как работать с record. не получается вытянуть свойства (ID) */
-
-    var html = '<a href="#/master/contract">' + data + '</a>';
-    return html;
-  }
-
   @override
   void ngOnInit() {
-    var columns = new List<Column>();
-    columns.add(new Column(field: 'number', caption: '№', size: '150px'));
-    columns.add(new Column(
-        field: 'name',
-        caption: 'Наименование договора',
-        size: '300px',
-        render: allowInterop(_render)));
-    columns.add(new Column(
-        field: 'conclusionDate',
-        caption: 'Дата заключения',
-        size: '150px',
-        render: 'date'));
-
-    GridOptions options = new GridOptions()
-      ..name = 'grid'
-      ..fixedBody = true
-      ..columns = columns
-      ..url = '//cm-ylng-msk-01/cmas-backend/api/contract'
-      //..url = 'http://localhost:5000/api/contract'      
-      ..method = 'GET';
-
-    _grid = new Grid(this._resourcesLoaderService, "#grid", options);
-  }
-
-  @override
-  void ngOnDestroy() {
-    _grid.Destroy();
+    var table = querySelector('[table-click]') as TableElement;
+    table.rows.forEach((TableRowElement row) {
+      row.onClick.listen((MouseEvent e) {
+        _router.navigateByUrl(e.currentTarget.getAttribute('data-href'));
+      });
+    });
   }
 }
